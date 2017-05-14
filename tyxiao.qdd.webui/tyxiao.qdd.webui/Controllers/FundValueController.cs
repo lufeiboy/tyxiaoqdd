511cc8963a246311dadaba02842966a2826cfc44
lufeiboy <lufeidboy@163.com>
2017-05-14 16:20:07 +0800

    xinz yige wenj

---
+++ tyxiao.qdd.webui/tyxiao.qdd.webui/Controllers/FundValueController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace tyxiao.qdd.webui.Controllers
+{
+    public class FundValueController : Controller
+    {
+        //
+        // GET: /FundValue/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
